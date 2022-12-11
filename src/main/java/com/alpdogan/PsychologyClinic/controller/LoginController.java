89ad937b45d1f4b55e03ea3363af89cafe2ff85f
Alptuğ Doğan <alpdogan000@gmail.com>
2022-12-11 12:39:45 +0300

    Created controllers for login, home and dashboard pages

---
+++ src/main/java/com/alpdogan/PsychologyClinic/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.alpdogan.PsychologyClinic.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping("/login")
+    public ModelAndView showLoginPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+}
