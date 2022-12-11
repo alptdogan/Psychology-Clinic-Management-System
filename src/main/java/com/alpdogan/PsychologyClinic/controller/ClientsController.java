bcec8cf7eb887b884cd2ffb9d16b5a1fd9b6b296
Alptuğ Doğan <alpdogan000@gmail.com>
2022-12-11 12:38:02 +0300

    Created ClientsController

---
+++ src/main/java/com/alpdogan/PsychologyClinic/controller/ClientsController.java
@@ -0,0 +1,87 @@
+package com.alpdogan.PsychologyClinic.controller;
+
+import com.alpdogan.PsychologyClinic.entity.Clients;
+import com.alpdogan.PsychologyClinic.service.ClientsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/clients")
+public class ClientsController {
+
+    @Autowired
+    private ClientsService clientsService;
+
+    @GetMapping
+    public String displayClients (Model model) {
+
+        List<Clients> clients = clientsService.getAllClients();
+        model.addAttribute("clients", clients);
+
+        return "list-clients";
+
+    }
+
+    @GetMapping("/new")
+    public String displayClientForm (Model model) {
+
+        Clients clients = new Clients();
+        model.addAttribute("clients", clients);
+
+        return "new-client";
+
+    }
+
+    @GetMapping("/register")
+    public String registerClient(Model model) {
+
+        Clients clients = new Clients();
+        model.addAttribute("therapist", clients);
+
+        return "new-client";
+
+    }
+
+    @PostMapping("/addClient")
+    public String createClient (@ModelAttribute("clients") Clients client) {
+
+        clientsService.createClient(client);
+
+        return "redirect:/clients";
+
+    }
+
+    @GetMapping("/update")
+    public String displayClientUpdateForm(@RequestParam("id") int id, Model model) {
+
+        Clients clients = clientsService.getClientById(id);
+        model.addAttribute("clients", clients);
+
+        return "new-client";
+
+    }
+
+    @GetMapping("/delete")
+    public String deleteClient(@RequestParam("id") int id, Model model) {
+
+        clientsService.deleteClientById(id);
+
+        return "redirect:/clients";
+
+    }
+
+    @GetMapping("/details")
+    public String displayClientDetails(@RequestParam("id") int id, Model model) {
+
+        Clients clients = clientsService.getClientById(id);
+        model.addAttribute("clients", clients);
+
+        return "clients-details";
+
+    }
+
+}
