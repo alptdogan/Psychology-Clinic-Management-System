85af4283820e88a18aa903ae0b40feaf4941db98
Alptuğ Doğan <alpdogan000@gmail.com>
2022-12-13 16:39:53 +0300

    Updated SecurityConfiguration.java

--- src/main/java/com/alpdogan/PsychologyClinic/configuration/SecurityConfiguration.java
+++ src/main/java/com/alpdogan/PsychologyClinic/configuration/SecurityConfiguration.java
@@ -21,11 +21,7 @@ public class SecurityConfiguration {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails client = User.withDefaultPasswordEncoder()
-                .username("client")
-                .password("client")
-                .roles("CLIENT")
-                .build();
+
         UserDetails therapist = User.withDefaultPasswordEncoder()
                 .username("therapist")
                 .password("therapist")
@@ -36,11 +32,13 @@ public class SecurityConfiguration {
                 .password("admin")
                 .roles("ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(client, therapist, admin);
+        return new InMemoryUserDetailsManager(therapist, admin);
+
     }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeRequests(auth -> {
@@ -58,6 +56,7 @@ public class SecurityConfiguration {
                 })
                 .httpBasic(withDefaults())
                 .build();
+
     }
 
 }
