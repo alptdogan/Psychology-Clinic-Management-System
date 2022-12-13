3ed8727fc910708e31107a60feaca4d600f86043
Alptuğ Doğan <alpdogan000@gmail.com>
2022-12-13 15:44:13 +0300

    Updated html files

--- src/main/java/com/alpdogan/PsychologyClinic/entity/Clients.java
+++ src/main/java/com/alpdogan/PsychologyClinic/entity/Clients.java
@@ -20,10 +20,9 @@ public class Clients {
     @Column (name = "id")
     private int id;
 
-    @Column(name = "first_name")
-    private String firstName;
-    @Column(name = "last_name")
-    private String lastName;
+    @Column(name = "full_name")
+    private String fullName;
+
     @Column(name = "email")
     private String email;
 
